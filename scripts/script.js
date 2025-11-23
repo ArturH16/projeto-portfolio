4321c7c83ec7d9b55371e32067c1847ee531a99b
ArturH16 <holandaartur163@gmail.com>
2025-11-23 15:09:51 -0300

    Adição do modo escuro para o site

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,13 @@
 const botao = document.getElementById('botao-tema');
 const body = document.body;
+const root = document.documentElement
+const estilosComputados = window.getComputedStyle(root);
+const valorOriginalFundoCard = estilosComputados.getPropertyValue('--fundo-card')
+const valorOriginalDestaque = estilosComputados.getPropertyValue('--destaque')
+const valorOriginalFundoPrincipal = estilosComputados.getPropertyValue('--fundo-principal')
+const ValorOriginalTexto = estilosComputados.getPropertyValue('--texto')
+const ValorOriginalTextoInverso = estilosComputados.getPropertyValue('--texto-inverso')
+const ValorOriginalDestaqueInverso = estilosComputados.getPropertyValue('--destaque-inverso')
 
 // Persistência do tema
 const temasalvo = localStorage.getItem('tema');
@@ -9,9 +17,21 @@ temaEscuro(temasalvo === 'escuro');
 function temaEscuro(tipo) {
   if (tipo == true) {
     body.classList.add('escuro');
+    root.style.setProperty('--fundo-card','#383B38')
+    root.style.setProperty('--destaque','#FC4A1C')
+    root.style.setProperty('--fundo-principal','#222222')
+    root.style.setProperty('--texto',ValorOriginalTextoInverso)
+    root.style.setProperty('--destaque-inverso',ValorOriginalTexto)
+    root.style.setProperty('--texto-inverso',ValorOriginalTexto)
     botao.innerHTML = '<i class="fa-solid fa-sun"></i>';
   } else {
     body.classList.remove('escuro');
+    root.style.setProperty('--fundo-card',valorOriginalFundoCard)
+    root.style.setProperty('--destaque',valorOriginalDestaque)
+    root.style.setProperty('--fundo-principal',valorOriginalFundoPrincipal)
+    root.style.setProperty('--texto',ValorOriginalTexto)
+    root.style.setProperty('--destaque-inverso',ValorOriginalDestaqueInverso)
+    root.style.setProperty('--texto-inverso',ValorOriginalTextoInverso)
     botao.innerHTML = '<i class="fa-solid fa-moon"></i>';
   }
 }
